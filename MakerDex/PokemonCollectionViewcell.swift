9ddda869a4c74795138a60ba7a2e59f47f737232
Keri <keri.baba@pucp.pe>
2017-04-08 19:09:29 -0500

    Todo background + barra + icono + tabla

---
+++ MakerDex/PokemonCollectionViewcell.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonCollectionViewcell.swift
+//  MakerDex
+//
+//  Created by Keri on 4/8/17.
+//  Copyright © 2017 sungkim.tech. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCollectionViewcell: UICollectionViewCell {
+    
+}
