1cdf97d02b54e61374111d0ea2f8810de13da705
Sung Kim <kim_sung09@live.com>
2017-04-08 19:08:11 -0500

    agrego mas cosas

---
+++ MakerDex/PokemonCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonCollectionViewCell.swift
+//  MakerDex
+//
+//  Created by Sung Kim on 4/8/17.
+//  Copyright © 2017 sungkim.tech. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCollectionViewCell: UICollectionViewCell {
+    
+}
--- MakerDex/ViewController.swift
+++ MakerDex/ViewController.swift
@@ -8,16 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    //MARK: - Properties
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: - Functions
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pokemonCollectionViewCell", for: indexPath) as! PokemonCollectionViewCell
+        
+        return cell
     }
 }
 
